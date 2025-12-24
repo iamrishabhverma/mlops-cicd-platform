b8acc1cb2736e94d29a2ce7595b9a34613be2000
iamrishabhverma <46955354+iamrishabhverma@users.noreply.github.com>
2025-12-24 01:59:53 -0400

    testing bash

--- scripts/slack_notify.sh
+++ scripts/slack_notify.sh
@@ -2,7 +2,7 @@
 
 COMMIT_MESSAGE='Sample commit message with special characters: "Quotes", new lines
 and $dollar signs.'
-# COMMIT_MESSAGE='Fix "model" bug'
+
 PAYLOAD=$(jq -n --arg text "✅ ML API built & deployed successfully!\n\n📝 Commit: $COMMIT_MESSAGE" '{text: $text}')
 echo "$PAYLOAD" | jq .
 
