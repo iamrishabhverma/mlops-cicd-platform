aea1d2125f26e247c5e8e5cf10cd6230a710cd2d
iamrishabhverma <46955354+iamrishabhverma@users.noreply.github.com>
2025-12-24 00:56:49 -0400

    Slack Commit and bash test

--- scripts/slack_notify.sh
+++ scripts/slack_notify.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"ML Pipeline Notification"}' $SLACK_WEBHOOK_URL
+
+COMMIT_MESSAGE='Sample commit message with special characters: "Quotes", new lines
+and $dollar signs.'
+# COMMIT_MESSAGE='Fix "model" bug'
+PAYLOAD=$(jq -n --arg text "✅ ML API built & deployed successfully!\n\n📝 Commit: $COMMIT_MESSAGE" '{text: $text}')
+echo "$PAYLOAD" | jq .
+
